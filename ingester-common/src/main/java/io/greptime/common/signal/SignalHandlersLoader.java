12dcde84dfdf2d1fbbe4843d090063d7631a889f
tison <wander4096@gmail.com>
2024-03-19 09:00:15 +0800

    reformat license header
    Signed-off-by: tison <wander4096@gmail.com>

--- ingester-common/src/main/java/io/greptime/common/signal/SignalHandlersLoader.java
+++ ingester-common/src/main/java/io/greptime/common/signal/SignalHandlersLoader.java
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.greptime.common.signal;
 
 import io.greptime.common.Keys;
