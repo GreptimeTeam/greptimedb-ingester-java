ba6ebb014de8015c3cef53fbd72e545871bde0a5
jeremyhi <jiachun_fjc@163.com>
2025-05-14 13:07:00 +0800

    release: v0.14.3 (#79)

--- ingester-protocol/src/test/java/io/greptime/UtilTest.java
+++ ingester-protocol/src/test/java/io/greptime/UtilTest.java
@@ -27,6 +27,6 @@ public class UtilTest {
     @Test
     public void testClientVersion() {
         String ver = Util.clientVersion();
-        Assert.assertEquals("0.14.2", ver);
+        Assert.assertEquals("0.14.3", ver);
     }
 }
